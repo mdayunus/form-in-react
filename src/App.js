49432ac8bf56f7f48f2879293291acf2f4c7deb4
mdayunus <mdayunus@gmail.com>
2020-08-20 23:07:09 +0530

    form-in-eact-eg

--- src/App.js
+++ src/App.js
@@ -1,26 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firstname: "",
+      lastname: "",
+      married: false,
+      gender: "",
+      favColor: "",
+      govDoc: {
+        aadhar: false,
+        passport: false,
+      },
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    if (type === "checkbox") {
+      if (name === "aadhar" || name === "passport") {
+        this.setState((prevState) => {
+          const ns = prevState;
+          ns.govDoc[name] = checked;
+          return ns;
+        });
+      } else {
+        this.setState({
+          [name]: checked,
+        });
+      }
+    } else {
+      this.setState({
+        [name]: value,
+      });
+    }
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          name="firstname"
+          value={this.state.firstname}
+          placeholder="first name"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="lastname"
+          value={this.state.lastname}
+          placeholder="last name"
+          onChange={this.handleChange}
+        />
+        <br />
+        <label>Married</label>
+        <input
+          type="checkbox"
+          name="married"
+          value={this.state.married}
+          onChange={this.handleChange}
+        />
+        <br />
+        <label>gender</label>
+        <br />
+        <input
+          type="radio"
+          name="gender"
+          value="female"
+          checked={this.state.gender === "female"}
+          onChange={this.handleChange}
+        />
+        <label>Female</label>
+
+        <input
+          type="radio"
+          name="gender"
+          value="male"
+          checked={this.state.gender === "male"}
+          onChange={this.handleChange}
+        />
+        <label>Male</label>
+
+        <br />
+        <label>fav color</label>
+        <select
+          name="favColor"
+          value={this.state.favColor}
+          onChange={this.handleChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+          <option value="green">Green</option>
+          <option value="yellow">Yellow</option>
+        </select>
+
+        <br />
+
+        <label>
+          <input
+            type="checkbox"
+            name="aadhar"
+            checked={this.state.govDoc.aadhar}
+            onChange={this.handleChange}
+          />
+          Aadhar
+          <input
+            type="checkbox"
+            name="passport"
+            checked={this.state.govDoc.passport}
+            onChange={this.handleChange}
+          />
+          Passport
+        </label>
+        <h1>
+          {this.state.firstname} {this.state.lastname} {this.state.married}
+          {this.state.gender} {this.state.favColor} {this.state.govDoc.aadhar}
+          {this.state.govDoc.passport}
+        </h1>
+      </form>
+    );
+  }
 }
 
 export default App;
